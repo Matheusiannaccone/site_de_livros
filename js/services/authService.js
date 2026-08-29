256910092e0a4fd5cda84244a33fdfd53e1541db
Matheusiannaccone <matheusiannaccone@outlook.com>
2026-08-29 18:32:36 -0300

    0.5.2 - chore: cria estrutura inicial de authService e seleção de datasource

---
+++ js/services/authService.js
@@ -0,0 +1,23 @@
+export async function signUp({ email, password, username, displayName }) {
+  throw new Error("Not implemented");
+}
+
+export async function signIn({ email, password }) {
+  throw new Error("Not implemented");
+}
+
+export async function signOut() {
+  throw new Error("Not implemented");
+}
+
+export async function getSession() {
+  throw new Error("Not implemented");
+}
+
+export async function getCurrentUser() {
+  throw new Error("Not implemented");
+}
+
+export function onAuthStateChange(callback) {
+  throw new Error("Not implemented");
+}
